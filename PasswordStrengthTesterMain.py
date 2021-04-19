ac0478f24774ac7768eee2fd08d4d5e0de34285d
evillar1 <57238970+evillar1@users.noreply.github.com>
2021-04-18 21:14:13 -0400

    adding initial script that will run,
    This code runs the page, the page gives options and currently will return the password inputted. This will be the heart of the strength tester.

--- PasswordStrengthTesterMain.py
+++ PasswordStrengthTesterMain.py
@@ -1,1 +1,21 @@
 #My main file for code
+from flask import Flask, render_template, request app=Flask(__name__)
+@app.route('/greet',methods=['POST'])
+
+@app.route('/')
+def home():
+   return render_template("home.html",myName="")
+  
+@app.route('/about/')
+def about():
+  return render_template("about.html")
+
+if __name__=="__main__":
+  app.run(debug=True)
+  
+@app.route('/greet',methods=['POST'])
+def passtest():
+  inputPass = request.form[myPass']
+  ip = request.remote_addr
+  inputPass = inputPass.upper()+" is the password inputted from " + str(ip)
+  return render_template("home.html",myPass=inputPass)
