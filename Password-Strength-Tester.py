08fdd8827f03f0ba73c35fbeddd4bcf9c2480e5a
evillar1 <57238970+evillar1@users.noreply.github.com>
2021-05-01 20:00:35 -0400

    Final update to password strength tester
    This is the final iteration of the password strength tester.

--- Password-Strength-Tester.py
+++ Password-Strength-Tester.py
@@ -6,14 +6,46 @@ app=Flask(__name__)
 @app.route('/greet', methods=['POST'])
 def greet():
     inputName = request.form['myName']
+
+    #split password given into list, separated by commas, and set var for password score
+    inputName.split(",")
+    strength_score = 0
     
-    #tester here
-    strength_score = 100
+    #gathers points based on what the password is comprised of, character wise
+    for character in inputName:
+        if character.isalpha():
+            if character.isupper():
+                strength_score += 2
+            else:   
+                strength_score += 1
+        elif character.isdigit():
+            strength_score += 2
+        else:
+            strength_score += 6
+
+    #gathers points based on how long password is
+    if len(inputName) < 5:
+        strength_score += 3
+    elif len(inputName) >= 5 and len(inputName) < 8:
+        strength_score += 6
+    elif len(inputName) >= 8 and len(inputName) < 11:
+        strength_score += 9
+    else:
+        strength_score += 12
+
+    #gathers score for password based on points collected
+    if strength_score < 15:
+        final_score = "Insufficent..."
+    elif strength_score >=15 and strength_score < 25:
+        final_score = "Sufficent."
+    else:
+        final_score = "Excellent!"
     
-    inputName ="\'" + inputName + "\' is the password you inputted, and it has a strength of " + str(strength_score) + " out of 100!"
+    #prints password given, points score, and final score word
+    inputName ="\'" + inputName + "\' is the password you inputted, and it has \na strength of " + str(strength_score) + "! " + final_score
     return render_template("home.html",myName=inputName)
 
-@app.route('/')
+@app.route('/') 
 def home():
     return render_template("home.html",myName="")
 
@@ -23,3 +55,4 @@ def about():
 
 if __name__=="__main__":
     app.run(debug=True)
+    
